9f400ce0f3ad8b650736a737199f74e46ed1be11
vriskaserket51 <serketvriska51@gmail.com>
2024-08-25 22:52:15 +0900

    edit fs

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { Eastward } from "@/eastward";
 export * from "@/asset/font";
 export * from "@/asset/texture";
 
+export { initBrowser } from "@/util/filesystem";
 export * from "@/util/hmg";
