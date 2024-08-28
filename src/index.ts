3a68660970e9de402b316420ceb69ee6ef031b0c
vriskaserket51 <serketvriska51@gmail.com>
2024-08-29 01:37:29 +0900

    add sq support

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Eastward } from "@/eastward";
 
 export * from "@/asset/font";
 export * from "@/asset/locale";
+export * from "@/asset/sq";
 export * from "@/asset/texture";
 
 export { initBrowser } from "@/util/filesystem";
