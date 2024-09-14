d988ef0c4847e2ba554bd8ff8a36d85a2cafcd38
vriskaserket51 <serketvriska51@gmail.com>
2024-09-14 16:39:33 +0900

    add mesh asset

---
+++ src/asset/mesh.ts
@@ -0,0 +1,40 @@
+import { Asset, AssetNode } from "@/asset/node";
+import { Eastward } from "@/eastward";
+import { writeFileSync } from "fs";
+import fs from "fs/promises";
+
+export class MeshAsset extends Asset {
+  def: string | null = null;
+
+  constructor(eastward: Eastward, node: AssetNode) {
+    super(eastward, node);
+  }
+
+  get type(): string {
+    return Asset.Type.Text;
+  }
+
+  async toString(): Promise<string | null> {
+    return this.def;
+  }
+
+  async load() {
+    // TODO
+  }
+
+  async saveFile(filePath: string) {
+    if (!this.def) {
+      return;
+    }
+    super.beforeSave(filePath);
+    await fs.writeFile(filePath, this.def);
+  }
+
+  saveFileSync(filePath: string) {
+    if (!this.def) {
+      return;
+    }
+    super.beforeSave(filePath);
+    writeFileSync(filePath, this.def);
+  }
+}
