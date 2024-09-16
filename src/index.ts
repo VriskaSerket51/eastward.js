028630608eafd3cd47d9f779195e1525a11160ab
vriskaserket51 <serketvriska51@gmail.com>
2024-09-16 21:59:33 +0900

    edit export

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ export * from "@/asset/node";
 
 export * from "@/asset/animator-data";
 export * from "@/asset/asset-map";
-export * from "@/asset/code-tileset";
 export * from "@/asset/data";
 export * from "@/asset/deck-2d";
 export * from "@/asset/effect";
+export * from "@/asset/file";
 export * from "@/asset/font";
 export * from "@/asset/glsl";
 export * from "@/asset/locale";
@@ -16,6 +16,7 @@ export * from "@/asset/lua";
 export * from "@/asset/m-sprite";
 export * from "@/asset/material";
 export * from "@/asset/mesh";
+export * from "@/asset/movie";
 export * from "@/asset/physics";
 export * from "@/asset/prefab";
 export * from "@/asset/render-target";
@@ -27,6 +28,7 @@ export * from "@/asset/story-graph";
 export * from "@/asset/stylesheet";
 export * from "@/asset/text";
 export * from "@/asset/texture";
+export * from "@/asset/tileset";
 export * from "@/asset/ui-style";
 
 export * from "@/util/hmg";
