2c7b658da32c1f2572140d1b2c484eb74702bc98
vriskaserket51 <serketvriska51@gmail.com>
2024-09-19 17:50:55 +0900

    ml test

--- src/asset/scheme.ts
+++ src/asset/scheme.ts
@@ -1,10 +1,12 @@
 import { Asset, AssetNode } from "@/asset/node";
 import { Eastward } from "@/eastward";
+import { generateGraphML } from "@/util/fsm";
+import { Lua } from "wasmoon-lua5.1";
 import { writeFileSync } from "fs";
 import fs from "fs/promises";
 
 export class FSMSchemeAsset extends Asset {
-  def: string | null = null;
+  ml: string | null = null;
 
   constructor(eastward: Eastward, node: AssetNode) {
     super(eastward, node);
@@ -15,27 +17,41 @@ export class FSMSchemeAsset extends Asset {
   }
 
   async toString(): Promise<string | null> {
-    return this.def;
+    return this.ml;
   }
 
   async load() {
-    this.def = await this.eastward.loadTextFile(this.node.objectFiles!.def);
+    const def = await this.eastward.loadTextFile(this.node.objectFiles!.def);
+    if (!def) {
+      return;
+    }
+    this.ml =def;
+
+    // const lua = await Lua.create();
+    // const value = await lua.doString(def);
+    // const fsm: Map<string, any> = value.$detach();
+    // lua.global.close();
+
+    // function helper(map: Map<string, any>) {
+    //   for (const key of map.keys()) {
+    //     const value = map.get(key);
+    //     if (value instanceof Map) {
+    //       helper(value);
+    //       map.set(key, Object.fromEntries(value));
+    //     }
+    //   }
+    // }
+
+    // helper(fsm);
+    // this.ml = generateGraphML(Object.fromEntries(fsm));
   }
 
   async saveFile(filePath: string) {
-    if (!this.def) {
+    if (!this.ml) {
       return;
     }
     super.beforeSave(filePath);
-    await fs.writeFile(filePath, this.def);
-  }
-
-  saveFileSync(filePath: string) {
-    if (!this.def) {
-      return;
-    }
-    super.beforeSave(filePath);
-    writeFileSync(filePath, this.def);
+    await fs.writeFile(filePath, this.ml);
   }
 }
 
